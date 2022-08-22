e000a5cfa4ac0e41f1490a66fb2bfb8868c8ffe1
kobbejager <wfremout+github@gmail.com>
2022-08-22 15:37:40 +0200

    Add crude api endpoints for schema CRUD

--- src/routers/api.py
+++ src/routers/api.py
@@ -6,8 +6,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import ValidationError
 
 import core
-import core.utils.jsonschema
-import models
+#import core.utils.jsonschema
+#import models
 import crud
 
 
@@ -22,82 +22,25 @@ client = AsyncIOMotorClient(core.settings.mongo_conn_str)
 db = client[core.settings.mongo_db]
 
 
-#
-#   DATASET ROUTES
-#
-
-@router.get("")
-async def search_collections(
-        skip: Optional[int] = Query(0, description="Skip the x first results"),
-        limit: Optional[int] = Query(10, description="Return x results"), 
-        find: Optional[str] = Query(None, description="Mongodb-style find query in JSON"),
-        sort_by: Optional[List[str]] = Query(["collection_code"], description="Sorting options (array of strings)"),
-        sort_desc: Optional[List[bool]] = Query([], description="Sort descending (arry of booleans)")):
+@router.get("/schema")
+async def get_all_schemas():
     """
-    Return all collections.
+    Return all schemas.
     """
-
-    if find is not None:
-        find = json.loads(find)
-    else:
-        find = {}
-
-    if len(sort_desc) > 0 and len(sort_desc) != len(sort_by):
-        raise HTTPException(status_code=400, detail="ParameterError")
-    try: 
-        response = await crud.collection.search(
-            collection=db.collections,
-            find=find,
-            skip=skip,
-            limit=limit,
-            sort_by=sort_by,
-            sort_desc=sort_desc)
-    except BaseException as err:
-        raise HTTPException(status_code=400, detail=str(err))
+    response = await crud.schema.search(
+        collection=db.schemas)
     return response
 
 
-@router.get("/list", response_model=List[models.CollectionCompact])
-async def get_collection_list():
+@router.get("/schema/{id}")
+async def get_schema_by_id(
+        id: str = Path(None, description="The id of the schema")):
     """
-    Return a list of all collections.
-    """
-    try:
-        response = await crud.collection.get_all(
-            collection=db.collections,
-            sort_by=["collection_name"],
-            limit=0)
-    except BaseException as err:
-        raise HTTPException(status_code=400, detail=str(err))
-    return response["data"]
-
-
-@router.get("/keys")
-async def get_collection_by_its_unique_keys(
-        collection_name: str = Query(..., description="Collection name")):
-    """
-    Return a single collection by its unique keys.
+    Return a single schema by its id.
     """
     try:
-        response = await crud.collection.get_by_keys(
-            collection=db.collections,
-            collection_name=collection_name)
-    except crud.NoResultsError:
-        raise HTTPException(status_code=404, detail="NoResults")
-    except BaseException as err:
-        raise HTTPException(status_code=400, detail=str(err))
-    return response
-
-
-@router.get("/{id}")
-async def get_collection_by_id(
-        id: str = Path(None, description="The id of the collection")):
-    """
-    Return a single collection by its id.
-    """
-    try:
-        response = await crud.collection.get(
-            collection=db.collections, 
+        response = await crud.schema.get(
+            collection=db.schemas, 
             id=id)
     except crud.NoResultsError:
         raise HTTPException(status_code=404, detail="NoResults")
@@ -106,24 +49,24 @@ async def get_collection_by_id(
     return response
 
 
-@router.post("")
-async def create_collection(collection: dict):
+@router.post("/schema")
+async def create_schema(schema: dict):
     """
-    Create a new collection.
+    Create a new schema.
     """
     try:
         # validate agains resource and category models
-        collection_resource_instance = models.CollectionUpdate(**collection)
-        await core.utils.jsonschema.validate_instance(collection, validate_category=True)
+        #schema_instance = models.SchemaUpdate(**schema)
+        #await core.utils.jsonschema.validate_instance(schema, validate_category=True)
 
-        # create the collection
-        response = await crud.collection.create(
-            collection=db.collections,
-            data=collection)
-    except ValidationError as err:
-        raise HTTPException(status_code=422, detail=err.errors())
-    except core.utils.jsonschema.SchemaValidationError as err:
-        raise HTTPException(status_code=422, detail=err.args[0])
+        # create the schema
+        response = await crud.schema.create(
+            collection=db.schemas,
+            data=schema)
+    # except ValidationError as err:
+    #     raise HTTPException(status_code=422, detail=err.errors())
+    # except core.utils.jsonschema.SchemaValidationError as err:
+    #     raise HTTPException(status_code=422, detail=err.args[0])
     except crud.DuplicateKeyError:
         raise HTTPException(status_code=422, detail="DuplicateKey")
     except crud.NotCreatedError:
@@ -133,27 +76,27 @@ async def create_collection(collection: dict):
     return response
 
 
-@router.put("/{id}")
-async def replace_collection(
-        collection: dict,
-        id: str = Path(None, description="The id of the collection")):
+@router.put("/schema/{id}")
+async def replace_schema(
+        schema: dict,
+        id: str = Path(None, description="The id of the schema")):
     """
-    Replace a collection (full update).
+    Replace a schema (full update).
     """
     try:
         # validate agains resource and category models
-        collection_resource_instance = models.CollectionUpdate(**collection)
-        await core.utils.jsonschema.validate_instance(collection, validate_category=True)
+        #collection_resource_instance = models.CollectionUpdate(**collection)
+        #await core.utils.jsonschema.validate_instance(collection, validate_category=True)
 
-        # update the collection
-        updated = await crud.collection.cascading_replace(
-            collection=db.collections, 
+        # update the schema
+        updated = await crud.schema.replace(
+            collection=db.schemas, 
             id=id,
-            data=collection)
-    except ValidationError as err:
-        raise HTTPException(status_code=422, detail=err.errors())
-    except core.utils.jsonschema.SchemaValidationError as err:
-        raise HTTPException(status_code=422, detail=err.args[0])
+            data=schema)
+    # except ValidationError as err:
+    #     raise HTTPException(status_code=422, detail=err.errors())
+    # except core.utils.jsonschema.SchemaValidationError as err:
+    #     raise HTTPException(status_code=422, detail=err.args[0])
     except crud.NoResultsError:
         raise HTTPException(status_code=404, detail="NoResults")
     except crud.DuplicateKeyError:
@@ -165,15 +108,15 @@ async def replace_collection(
     return updated
 
 
-@router.delete("/{id}")
-async def delete_collection(
-        id: str = Path(None, description="The id of the collection")):
+@router.delete("/schema/{id}")
+async def delete_schema(
+        id: str = Path(None, description="The id of the schema")):
     """
-    Delete a collection.
+    Delete a schema.
     """
     try:
-        deleted = await crud.collection.safe_remove(
-            collection=db.collections, 
+        deleted = await crud.schema.remove(
+            collection=db.schemas, 
             id=id)
     except crud.DependentObjectsError:
         raise HTTPException(status_code=400, detail="DependentObjects")
