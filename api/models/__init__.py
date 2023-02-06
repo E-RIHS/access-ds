042e824c938962d14adcfc35f6ee78799d00c722
kobbejager <wfremout+github@gmail.com>
2023-02-06 22:19:03 +0100

    update SchemaConfig model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,6 @@
-from .schema_config import SchemaConfig
 from .json_schema import JsonSchema, JsonSchemaShort, JsonSchemaUpdate, JsonSchemaList
 from .ui_schema import UiSchema, UiSchemaShort, UiSchemaUpdate, UiSchemaList
 from .i18n_schema import I18nSchema, I18nSchemaShort, I18nSchemaUpdate, I18nSchemaList
 from .default_dataset import DefaultDataset, DefaultDatasetShort, DefaultDatasetUpdate, DefaultDatasetList
+
+from .schema_config import SchemaConfig, SchemaConfigShort, SchemaConfigUpdate, SchemaConfigList
--- api/models/schema_config.py
+++ api/models/schema_config.py
@@ -1,17 +1,16 @@
-
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel, Field
 
 from core.enums import Resource
+from models.common import (
+    IdBaseModel, 
+    NameBaseModel, 
+    LoggingBaseModel, 
+    QueryParameters)
 
 
-class SchemaConfig(BaseModel):
-    id: str = Field(...)
-    name: str = Field(...,
-        description="Short descriptive name",
-        min_length=1,
-        max_length=20)
+class _SchemaConfigBase(BaseModel):
     resource: Resource = Field(...)
     category: str = Field(..., 
         description="Category identifier",
@@ -23,3 +22,20 @@ class SchemaConfig(BaseModel):
     schema_defaults: Optional[str] = Field(None)
     active: bool = Field(False)
     depreciated: bool = Field(False)
+
+
+class SchemaConfig(LoggingBaseModel, _SchemaConfigBase, NameBaseModel, IdBaseModel):
+    pass
+
+
+class SchemaConfigShort(_SchemaConfigBase, NameBaseModel, IdBaseModel):
+    pass
+
+
+class SchemaConfigUpdate(_SchemaConfigBase, NameBaseModel):
+    pass
+
+
+class SchemaConfigList(BaseModel):
+    query_parameters: QueryParameters = Field(...)
+    data: List[SchemaConfigShort] = Field([])
