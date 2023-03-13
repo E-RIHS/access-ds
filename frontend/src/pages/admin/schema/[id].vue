28cefa7435eb2ef9deb581773802a41f33adccdd
kobbejager <wim.fremout@kikirpa.be>
2023-03-13 17:28:34 +0100

    add main layout of config detail page

--- frontend/src/pages/admin/schema/[id].vue
+++ frontend/src/pages/admin/schema/[id].vue
@@ -14,9 +14,44 @@ export default {
         <w-titlebanner :title="this.$route.params.id"></w-titlebanner>
 
         <v-container>
-            <v-alert outlined text type="info">
-                Not yet implemented
-            </v-alert>
+            top
+        </v-container>
+
+        <v-container fluid>
+            <v-row>
+                <v-col>
+                    <v-card>
+                        <v-card-title>
+                            <v-tabs 
+                                v-model="tab" 
+                                v-if="!dataEntry"
+                                grow>
+                                <v-tab>JSON Schema</v-tab>
+                                <v-tab>UI Schema</v-tab>
+                                <v-tab>I18n Schema</v-tab>
+                                <v-tab>Default dataset</v-tab>
+                            </v-tabs>
+                        </v-card-title>
+                        <v-card-text v-if="tab === 0">
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card>
+                        <v-card-title>
+                                <v-tabs 
+                                v-model="tab" 
+                                v-if="!dataEntry"
+                                grow>
+                                <v-tab>Example form</v-tab>
+                                <v-tab>Example dataset</v-tab>
+                            </v-tabs>
+                        </v-card-title>
+                        <v-card-text v-if="tab === 0">
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
